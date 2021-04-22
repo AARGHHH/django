6ac3949652c20f205081f4b84a9b8ebd7deb8a6b
User <alice.burmaka@gmail.com>
2021-04-22 22:29:26 +0300

    -

--- gusto/settings.py
+++ gusto/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [BASE_DIR / 'templates'],
-        'DIRS': [os.path.join(BASE_DIR, 'gusto/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

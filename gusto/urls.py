688f3452b70cd17c8e3ce100bce623063a6ede9d
User <alice.burmaka@gmail.com>
2021-04-23 12:30:41 +0300

    -

--- gusto/urls.py
+++ gusto/urls.py
@@ -30,9 +30,6 @@ urlpatterns = [
     path('menu/', include('menu_gusto.urls')),
     path('events/', include('events.urls')),
     path('users_messages/', include('users_messages.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 

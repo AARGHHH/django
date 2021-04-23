7dc76ed87c8f509251dfa8b14786f52c1ba3039b
User <alice.burmaka@gmail.com>
2021-04-23 12:17:05 +0300

    -

--- gusto/settings.py
+++ gusto/settings.py
@@ -155,6 +155,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
